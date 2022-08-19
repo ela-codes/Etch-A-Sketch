6fadc7a63ff875dcf24f53175f91dfc759b92c4c
ateodocio <teodocio.am@gmail.com>
2022-08-19 07:15:11 -0500

    Implement grid size changes using user input

--- main.js
+++ main.js
@@ -1,26 +1,32 @@
-// create main grid container
 const grid = document.querySelector('.container')
+
+const resetButton = document.querySelector('button.reset')
 
 // default size of grid is 16 x 16
 let gridSize = 16
-// create a number of specified rows
-for (let i = 1; i <= gridSize; i++) {
-    const gridRow = document.createElement('div')
-    gridRow.classList.add('row')
-    grid.appendChild(gridRow)
-    // create number of specified boxes per row
-    for (let j = 1; j <= gridSize; j++) {
-        const box = document.createElement('div')
-        box.classList.add('row', `${i}`)
-        box.classList.add('box', `${j}`)
-        gridRow.appendChild(box)
+
+createGrid(gridSize)
+
+function createGrid(gridSize) {
+    // create a number of specified rows
+    for (let i = 1; i <= gridSize; i++) {
+        const gridRow = document.createElement('div')
+        gridRow.classList.add('row')
+        grid.appendChild(gridRow)
+        // create number of specified boxes per row
+        for (let j = 1; j <= gridSize; j++) {
+            const box = document.createElement('div')
+            box.classList.add('row', `${i}`)
+            box.classList.add('box', `${j}`)
+            gridRow.appendChild(box)
+        }
     }
+    const gridBoxes = document.querySelectorAll('.box')
+    
+    gridBoxes.forEach(box => box.addEventListener('mouseover', e => changeBoxColor(e.target)))  // highlights boxes
+
+    resetButton.addEventListener('click', () => resetBoxColor(gridBoxes)) 
 }
-
-// listen for a mouseover event for each box in the grid
-const gridBoxes = document.querySelectorAll('.box')
-
-gridBoxes.forEach(box => box.addEventListener('mouseover', e => changeBoxColor(e.target)))
 
 
 // function that applies the "hovered" styling to that box wherein a mouseover event was triggered
@@ -28,26 +34,33 @@ function changeBoxColor(currentBox) {
     currentBox.classList.add('hovered')
 }
 
-// a reset button that calls resestBoxColor() if clicked
-const resetButton = document.querySelector('button.reset')
-
-resetButton.addEventListener('click', () => resetBoxColor(gridBoxes))
-
-
 // function that removes the "hovered" styling to all boxes
 function resetBoxColor(gridBoxes) {
     gridBoxes.forEach(box => box.classList.remove('hovered'))
 }
 
-// listen for new grid size input, code setup but not yet working
 
-// function changeGridSize(event) {
-//     event.preventDefault();
-//     console.log(event)
-    
-// }
+// function that removes the current grid
+function removeGrid() {
+    const allRows = document.querySelectorAll('.row')
+    for (row of allRows) {
+        row.parentNode.removeChild(row)
+    }
+}
 
-// const userInput = document.querySelector('#inputSize')
 
-// userInput.addEventListener('submit', event => changeGridSize(event))
+// listen for new grid size input
+const userInput = document.getElementById('gridSize')
+userInput.addEventListener("keypress", event => changeGridSize(event))
+
+function changeGridSize(event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        removeGrid()
+        let newSize = Number(userInput.value)
+        if (newSize >= 2 && newSize <= 50) {
+            createGrid(newSize)
+        }
+    }
+}
 
